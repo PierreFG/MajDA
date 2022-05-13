7b22d8b325c1c43a786fcb55bc2670d0f2002409
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2022-05-13 14:09:53 +0200

    k

---
+++ majda_front/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  return (
+    <Container id="app-container">
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
